e87ce686bd8dbb58b8ff7260718045210d4bc2e0
Your Name <you@example.com>
2026-01-07 10:45:22 +0200

    Added operator ABC and supporting enum Created abstract base Operator class with calculate method that inheriting operators must implement. Added OperatorType enum for prefix/infix/postfix distinction. **deleted associativity due to an update regarding power behavior in this project**.

--- operator.py
+++ operator.py
@@ -3,11 +3,10 @@ from enums import OperatorType, Association
 
 
 class Operator(ABC):
-    def __init__(self, symbol: str, precedence: int, operator_type: OperatorType, association: Association):
+    def __init__(self, symbol: str, precedence: int, operator_type: OperatorType):
         self.symbol = symbol
         self.precedence = precedence
         self.operator_type = operator_type
-        self.association = association
 
     @abstractmethod
     def calculate(self, *operands: float) -> float:
